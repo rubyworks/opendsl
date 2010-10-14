af115b2f6f9e59aa4110fc96c8a11a72a1fa52ad
7rans <transfire@gmail.com>
2010-10-14 12:15:04 -0400

    minor: add version.rb and some comments

--- lib/opendsl.rb
+++ lib/opendsl.rb
@@ -1,6 +1,21 @@
-
-# The Stuff
+# OpenDSL is a clever way to create a plugable
+# free-form domain specific language.
+#
+#   Example = OpenDSL.new do
+#     size do
+#       100
+#     end
+#   end
+#
+#   class Foo
+#     include Example
+#   end
+#
+#   Foo.new.size  #=> 100
+#
 class OpenDSL < Module
+
+  require 'opendsl/version'
 
   #
   def initialize(&block)
---
+++ lib/opendsl/version.rb
@@ -0,0 +1,3 @@
+class OpenDSL
+  VERSION = "1.0.0"
+end
